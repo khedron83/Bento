821e18b72d1f73b1fc20b4400ba9040b7272b9c9
khedron83 <kane@cubicserenity.com>
2025-02-02 22:37:37 +0000

    Update config.js

--- config.js
+++ config.js
@@ -11,8 +11,8 @@ const CONFIG = {
 	// └─┘┴ ┴└─┘┴└─┘└─┘
 
 	// General
-	name: 'John',
-	imageBackground: false,
+	name: 'Stephen',
+	imageBackground: true,
 	openInNewTab: true,
 	twelveHourFormat: false,
 
